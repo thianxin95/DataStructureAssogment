dd11f168b320976235351813cfb0b56323fe9ea9
acer <acer@acer-PC>
2017-12-20 01:23:21 +0800

    my part

---
+++ src/JavaInterfaces/MaintainDM.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaInterfaces;
+
+/**
+ *
+ * @author acer
+ */
+public interface MaintainDM {
+    void CreateRecord();
+    void CheckDMInfo();
+    void UpdatDM();
+    
+}
